be147c2fb79e283ca27bcf5c9ec3e3a505df14c0
Karolis Rusenas <karolis.rusenas@gmail.com>
2020-06-13 18:36:44 +0100

    spec

--- Makefile
+++ Makefile
@@ -1,9 +1,34 @@
 SDK_VERSION = v0.18.1
 MACHINE = $(shell uname -m)
+BUILD_DIR = "build"
+YQ = $(BUILD_DIR)/yq
+GOLANGCI_LINT = $(BUILD_DIR)/golangci-lint
+OPERATOR_SDK = $(BUILD_DIR)/operator-sdk
+
+
+# Generate APIs, CRD specs and CRD clientset.
+go-gen:
+	$(OPERATOR_SDK) generate k8s
+	$(OPERATOR_SDK) generate crds
+
+##############################
+#     Third-party tools      #
+##############################
 
 operator-sdk:
 	# Download sdk only if it's not available.
-	@if [ ! -f operator-sdk ]; then \
-		curl -Lo operator-sdk https://github.com/operator-framework/operator-sdk/releases/download/$(SDK_VERSION)/operator-sdk-$(SDK_VERSION)-$(MACHINE)-linux-gnu && \
-		chmod +x operator-sdk; \
+	@if [ ! -f $(OPERATOR_SDK) ]; then \
+		curl -Lo $(OPERATOR_SDK) https://github.com/operator-framework/operator-sdk/releases/download/$(SDK_VERSION)/operator-sdk-$(SDK_VERSION)-$(MACHINE)-linux-gnu && \
+		chmod +x $(OPERATOR_SDK); \
+	fi
+
+yq: ## Install yq.
+	@if [ ! -f $(YQ) ]; then \
+		curl -Lo $(YQ) https://github.com/mikefarah/yq/releases/download/2.3.0/yq_linux_amd64 && \
+		chmod +x $(YQ); \
+	fi
+
+golangci-lint: ## Install golangci-lint
+	@if [ ! -f $(GOLANGCI_LINT) ]; then \
+		curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(BUILD_DIR) v1.27.0; \
 	fi
--- pkg/apis/forward/v1/webhookrelayforward_types.go
+++ pkg/apis/forward/v1/webhookrelayforward_types.go
@@ -4,21 +4,131 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // WebhookRelayForwardSpec defines the desired state of WebhookRelayForward
 type WebhookRelayForwardSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// SecretRefName is the name of the secret object that contains
+	// generated token from https://my.webhookrelay.com/tokens
+	// secret should have two fields:
+	// key    - your token key (a long UUID)
+	// secret - token secret, encrypted once generated and cannot be recovered from Webhook Relay.
+	// If secret is lost, just create a new token
+	SecretRefName string `json:"secretRefName,omitempty"`
+
+	// SecretRefNamespace is the namespace of the secret reference.
+	SecretRefNamespace string `json:"secretRefNamespace,omitempty"`
+
+	// Image is webhookrelayd container, defaults to webhookrelay/webhookrelayd:latest
+	Image string `json:"image,omitempty"`
+
+	// Buckets to manage and subscribe to. Each CR can control one or more buckets. Buckets can be inspected
+	// and manually created via Web UI here https://my.webhookrelay.com/buckets
+	Buckets []BucketSpec `json:"buckets"`
 }
+
+// BucketSpec defines a bucket that groups one or more inputs (public endpoints) and
+// one ore more outputs (where the webhooks should be routed)
+type BucketSpec struct {
+	// Ref is the name or the ID of a bucket that can be reused
+	// (if it already exists) or that will be created by the operator. Buckets
+	// act as a grouping mechanism for Inputs and Outputs
+	Ref string `json:"ref,omitempty"`
+
+	Description string `json:"description,omitempty"`
+
+	// Inputs are your public endpoints. Inputs can either be https://my.webhookrelay.com/v1/webhooks/[unique ID]
+	// format or custom subdomains under https://[subdomain].hooks.webhookrelay.com or
+	// completely custom domains such as https://hooks.example.com.
+	// Important! Note that if you specify inputs, operator will automatically synchronize inputs of the specified
+	// bucket with the provided CR spec.
+	Inputs []InputSpec `json:"inputs,omitempty"`
+
+	// Outputs are destinations where webhooks/API requests should be forwarded.
+	Outputs []OutputSpec `json:"outputs,omitempty"`
+}
+
+// InputSpec defines an input that belong to a bucket
+type InputSpec struct {
+	Name string `json:"name,omitempty"`
+
+	// FunctionID attaches function to this input. Functions on inputs can modify
+	// responses to the caller and modify requests that are then passed to each
+	// output.
+	FunctionID string `json:"function_id"`
+
+	// Static response configuration
+	ResponseHeaders    map[string][]string `json:"headers,omitempty"`
+	ResponseStatusCode int                 `json:"statusCode,omitempty"`
+	ResponseBody       string              `json:"body,omitempty"`
+
+	// Dynamic response configuration
+	// either output name, ID or "anyOutput" to indicate that the first response
+	// from any output is good enough. Defaults to empty string
+	ResponseFromOutput string `json:"responseFromOutput,omitempty"`
+
+	// CustomDomain can be used to assign a permanent domain name for your input
+	// such as example.hooks.webhookrelay.com
+	CustomDomain string `json:"customDomain,omitempty"`
+	// PathPrefix can be combined together with CustomDomain to create 'API like'
+	// functionality where calls from:
+	// petshop.com/dogs -> are forwarded to [dogs store]
+	// petshop.com/cats -> are forwarded to [cats store]
+	PathPrefix string `json:"pathPrefix,omitempty"`
+
+	// Description can be any string
+	Description string `json:"description,omitempty"`
+}
+
+// OutputSpec defines and output that belong to a bucket. Outputs are destinations
+// where webhooks/API requests are forwarded.
+type OutputSpec struct {
+	Name string `json:"name,omitempty"`
+
+	// FunctionID attaches function to this output. Functions on output can modify
+	// requests that are then passed to destinations.
+	FunctionID string `json:"function_id,omitempty"`
+
+	// OverrideHeaders
+	OverrideHeaders map[string]string `json:"overrideHeaders,omitempty"`
+
+	// Destination is a URL that specifies where to send the webhooks. For example it can be
+	// http://local-jenkins/ghpr for Jenkins webhooks or any other URL.
+	Destination string `json:"destination"`
+
+	// Internal specifies whether webhook should be sent to an internal destination. Since
+	// operator is working with internal agents, this option defaults to True
+	Internal *bool `json:"internal,omitempty"`
+
+	// Timeout specifies how long agent should wait for the response
+	Timeout int `json:"timeout"`
+
+	// Description can be any string
+	Description string `json:"description"`
+}
+
+// ForwarderPhase is the phase of the Webhook Relay forwarder node at a given point in time.
+type ForwarderPhase string
+
+// Constants for operator defaults values and different phases.
+const (
+	ForwarderPhaseInitial     ForwarderPhase = ""
+	ForwarderPhaseRunning     ForwarderPhase = "Running"
+	ForwarderPhaseCreating    ForwarderPhase = "Creating"
+	ForwarderPhasePending     ForwarderPhase = "Pending"
+	ForwarderPhaseTerminating ForwarderPhase = "Terminating"
+)
 
 // WebhookRelayForwardStatus defines the observed state of WebhookRelayForward
 type WebhookRelayForwardStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	Phase ForwarderPhase `json:"phase,omitempty"`
+	Ready bool           `json:"ready,omitempty"` // whether deployment is ready
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
